57ce6bf9640930a2514b9e81016b753f8a9698d7
jyasuu <s1002605@pu.edu.tw>
2022-06-26 11:31:35 +0000

    test greeting

---
+++ src/main/java/com/pouchen/scmocp/scmocpapi/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.pouchen.scmocp.scmocpapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.pouchen.scmocp.scmocpapi.service.GreetingService;
+
+
+@Controller
+public class GreetingController {
+
+	private final GreetingService service;
+
+	public GreetingController(GreetingService service) {
+		this.service = service;
+	}
+
+	@RequestMapping("/greeting")
+	public @ResponseBody String greeting() {
+		return service.greet();
+	}
+
+}
---
+++ src/main/java/com/pouchen/scmocp/scmocpapi/service/GreetingService.java
@@ -0,0 +1,10 @@
+package com.pouchen.scmocp.scmocpapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+	public String greet() {
+		return "Hello, World";
+	}
+}
