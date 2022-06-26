727f939f1651f9489332e23c412e21eca8393a0b
Leo.yen <leo.yen@gmail.com>
2022-06-26 17:43:40 +0800

    init

---
+++ src/main/java/com/pouchen/scmocp/scmocpapi/ScmocpapiApplication.java
@@ -0,0 +1,13 @@
+package com.pouchen.scmocp.scmocpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmocpapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmocpapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pouchen/scmocp/scmocpapi/ScmocpapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pouchen.scmocp.scmocpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmocpapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
